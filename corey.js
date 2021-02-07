3035c30ca31463c3257cee3c4947b7b40a5251a7
Dev <xhtush96@gmail.com>
2021-02-07 02:11:19 +0100

    adding a new file

---
+++ corey.js
@@ -0,0 +1,12 @@
+// I canceled adding to Git so I could show you.
+// This is a file we added just to add an unvevrsioned file to git
+// You can see in the commit dialog that the files that are already under source code control
+// are in one section and the fils that are not yet under source code control are in another section
+//By section, I mean, a different tree branch
+
+// Notice that the file that we just committeed is not in github yet
+//That's because we only committed it to the local repository that is on your computer
+// We need to push it to github for it to be there.
+// I wil do that now.
+
+// Now I will add this new file.
