1ee3a5e4c49ef9225abbd8d1ba84f48cc8ff62ab
Dev <xhtush96@gmail.com>
2021-02-07 15:03:48 +0100

    now that file is under source code control, so it is "versioned" so now we'll check in our changes. okay

--- corey.js
+++ corey.js
@@ -1,10 +1,10 @@
 // I canceled adding to Git so I could show you.
-// This is a file we added just to add an unvevrsioned file to git
+// This is a file we added just to add an unversioned file to git
 // You can see in the commit dialog that the files that are already under source code control
 // are in one section and the fils that are not yet under source code control are in another section
 //By section, I mean, a different tree branch
 
-// Notice that the file that we just committeed is not in github yet
+// Notice that the file that we just committed is not in github yet
 //That's because we only committed it to the local repository that is on your computer
 // We need to push it to github for it to be there.
 // I wil do that now.
@@ -14,8 +14,8 @@
 // Still not there, of course, cause we haven't piushed.
 // Pushing now.
 
-// Notoice that the file pushed to github did not have our most recent comments in it.
-// That 's because we neverfr checked in those most recent comments before pushing
+// Notice that the file pushed to github did not have our most recent comments in it.
+// That 's because we never checked in those most recent comments before pushing
 // So we pushed the previously checked in version.
 //
 // If you change a file, then check it in, then checge it again, then check it in
